981c482b7ac60c84b170b8bf74483c95cba97e30
monthakan-w <Monthakan4386@gmail.com>
2025-09-17 01:19:28 +0700

    7

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import App from './App.tsx'
 // import TouchList from './components/TodoList.tsx'
-// import Coures from './components/Course.tsx';
+import Coures from './components/Course.tsx';
 // import TodoApp from './components/TodoListHookForm.tsx';
 // import MemberList from './components/MemberList.tsx';
 
-import Govermment from './components/Govermment';
+// import Govermment from './components/Govermment';
 
 
 
@@ -20,13 +20,13 @@ createRoot(document.getElementById('root')!).render(
 
       {/* <TouchList /> */}
 
-      {/* <Coures/> */}
+      <Coures/>
 {/* 
       <MemberList members={[]} group="Bus"/>
       <MemberList members={[]} group="SajaBoys"/> */}
 
 
-      <Govermment />
+      {/* <Govermment /> */}
       
   </StrictMode>
 )
